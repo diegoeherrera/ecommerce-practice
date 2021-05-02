9c91b5b319a5dc3094d0ee24ec4bf4fc66e864e6
Diego Herrera <diego.herrera@globant.com>
2021-05-02 13:08:44 -0300

    router implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
-import Homepage from "./homepage.component";
+import { Route } from "react-router-dom";
+import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Route exact="/" path component={Homepage} />
+      <Route exact="/hats" path component={HatsPage} />
     </div>
   );
 }
